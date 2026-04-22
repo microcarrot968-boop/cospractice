f0c320f9092cfb1b24a66b30da41b2fdb67b3798
Jinju <microcarrot968@kentech.ac.kr>
2026-04-22 10:17:55 +0000

    update

---
+++ 7/human.h
@@ -0,0 +1,20 @@
+
+#ifndef __HUMAN_H__
+#define __HUMAN_H__
+
+class Human {
+private:
+    const char *name;
+    int age;
+
+public:
+    Human(const char *name, int age);
+    Human(const char *name);
+
+    const char *getName();
+    void setAge(int age);
+    int getAge();
+    void print();
+};
+
+#endif
---
+++ 7/main.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include "human.h"
+
+using namespace std;
+
+int main() {
+    Human *hw;
+
+    hw = new Human("Hyunwoo Lee");
+    hw->print();
+    hw->setAge(37);
+    hw->print();
+
+    return 0;
+}
